b6581f3f4714cf741c866083d9b6fde495bf712a
morgothB <iggorka123@gmail.com>
2018-03-10 01:08:30 +0300

    package change to "testpasckage"

--- src/stepic/lab4_5/Label.java
+++
@@ -1,5 +0,0 @@
-package stepic.lab4_5;
-
-public enum Label {
-    SPAM, NEGATIVE_TEXT, TOO_LONG, OK;
-}
--- src/stepic/lab4_5/TextAnalyzer.java
+++
@@ -1,6 +0,0 @@
-package stepic.lab4_5;
-
-interface TextAnalyzer {
-    Label processText (String text);
-}
-
